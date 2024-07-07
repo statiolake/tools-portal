ec5babaca9618cbed7ffc0941af512fccf222921
statiolake <statiolake@gmail.com>
2024-07-07 15:53:30 +0900

    fix: enable preprocess and add prerender

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
+  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
     paths: {
