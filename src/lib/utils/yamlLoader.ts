41874e2c41549e8c4b306d0a6018a2083f4248c9
statiolake <statiolake@gmail.com>
2024-07-07 15:58:36 +0900

    fix: support non-root base path

--- src/lib/utils/yamlLoader.ts
+++ src/lib/utils/yamlLoader.ts
@@ -1,9 +1,10 @@
+import { base } from '$app/paths';
 import type { Card } from '$lib/types';
 import yaml from 'js-yaml';
 
 export async function loadToolsConfig(): Promise<Card[]> {
   try {
-    const response = await fetch('/tools.yaml');
+    const response = await fetch(`${base}/tools.yaml`);
     const yamlText = await response.text();
     const config = yaml.load(yamlText) as Card[];
     return config;
